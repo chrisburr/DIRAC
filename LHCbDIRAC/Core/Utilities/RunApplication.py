64c68a87d3bd8b456b25e28e7390ea82544fa679
Federico Stagni <federico.stagni@cern.ch>
2019-07-23 12:17:12 +0200

    using DIRAC's function getNumberOfProcessors

--- LHCbDIRAC/Core/Utilities/RunApplication.py
+++ LHCbDIRAC/Core/Utilities/RunApplication.py
@@ -16,13 +16,13 @@ __RCSID__ = "$Id$"
 
 import sys
 import os
-import re
 import shlex
 
 from DIRAC import gConfig, gLogger
 from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
 from DIRAC.Core.Utilities.List import fromChar
 from DIRAC.Core.Utilities.Subprocess import systemCall
+from DIRAC.WorkloadManagementSystem.Utilities.JobParameters import getNumberOfProcessors
 
 
 class LbRunError(RuntimeError):
@@ -205,7 +205,7 @@ class RunApplication(object):
     # multicore?
     if self.multicore:
       if _multicoreWN():
-        nProcessors = _processorsWN()
+        nProcessors = getNumberOfProcessors()
         command += ' --ncpus %d ' % nProcessors
       else:
         self.log.info("Would have run with option '--ncpus', but it is not allowed here")
@@ -279,39 +279,3 @@ def _multicoreWN():
                    ''))
 
   return bool(tags and 'MultiProcessor' in tags)
-
-
-def _processorsWN():
-  """ Returns the number of processors on the WN (from JOBFEATURES)
-      If not present, looks in the CS for the #Processors tag, and for the WholeNode tag.
-      If nothing is found, just returns 1
-  """
-  # if JOBFEATURES is present
-  nProcessors = gConfig.getValue('/LocalSite/JOBFEATURES/allocated_cpu')
-  if nProcessors:
-    gLogger.info("Number of processors from /LocalSite/JOBFEATURES/allocated_cpu", nProcessors)
-    return int(nProcessors)
-
-  # now looking in the CS for #Processors tag
-  siteName = gConfig.getValue('/LocalSite/Site')
-  gridCE = gConfig.getValue('/LocalSite/GridCE')
-  queue = gConfig.getValue('/LocalSite/CEQueue')
-  # Tags of the CE
-  tags = fromChar(gConfig.getValue('/Resources/Sites/%s/%s/CEs/%s/Tag' % (siteName.split('.')[0], siteName, gridCE),
-                  ''))
-  # Tags of the Queue
-  tags += fromChar(gConfig.getValue('/Resources/Sites/%s/%s/CEs/%s/Queues/%s/Tag' % (siteName.split('.')[0], siteName,
-                                                                                     gridCE, queue),
-                   ''))
-  for tag in tags:
-    numberOfProcessorsTag = re.search('[0-9]Processors', tag)
-    if numberOfProcessorsTag:
-      gLogger.info("Number of processors from tags", nProcessors)
-      return int(numberOfProcessorsTag.string.replace('Processors', ''))
-
-  if 'WholeNode' in tags:
-    gLogger.info("Found WholeNode tag, returning -1")
-    return -1
-
-  gLogger.warn("Number of processors could not be found, returning just 1")
-  return 1
