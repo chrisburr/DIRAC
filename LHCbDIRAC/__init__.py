fe756e3aa3a8b7c3601a2ff7e826f2b1e38d3724
Zoltan Mathe <zoltan.mathe@cern.ch>
2019-09-26 15:11:28 +0200

    v10r0-pre1

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -27,10 +27,10 @@ rootPath = os.path.dirname(os.path.realpath(__path__[0]))
 
 # Define Version
 
-majorVersion = 9
-minorVersion = 4
-patchLevel = 4
-preVersion = 0
+majorVersion = 10
+minorVersion = 0
+patchLevel = 0
+preVersion = 1
 
 version = "v%sr%s" % (majorVersion, minorVersion)
 buildVersion = "v%dr%d" % (majorVersion, minorVersion)
