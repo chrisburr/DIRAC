921d9c11a12bbc7564067990d4ac6d6783016395
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2019-11-29 14:26:28 +0100

    Use the DFC recursive capability instead of looping

--- LHCbDIRAC/DataManagementSystem/Utilities/FCUtilities.py
+++ LHCbDIRAC/DataManagementSystem/Utilities/FCUtilities.py
@@ -16,67 +16,52 @@ from DIRAC import S_OK, S_ERROR, gLogger
 import sys, os
 from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
 
-def chown( directories, user = None, group = None, mode = None, recursive = False, ndirs = None, fcClient = None ):
+
+def chown(directories, user=None, group=None, mode=None, recursive=False, ndirs=None, fcClient=None):
   """
   This method may change the user, group or mode of a directory and apply it recursively if required
   """
   if ndirs is None:
     ndirs = 0
   if not directories:
-    return S_OK( ndirs )
-  if isinstance( directories, basestring ):
-    directories = directories.split( ',' )
+    return S_OK(ndirs)
+  if isinstance(directories, basestring):
+    directories = directories.split(',')
   if fcClient is None:
     fcClient = FileCatalogClient()
+  timeout = 3600 if recursive else 10 * len(directories)
   if user is not None:
-    res = fcClient.changePathOwner( dict.fromkeys( directories, user ) )
+    res = fcClient.changePathOwner(dict.fromkeys(directories, user), recursive=recursive, timeout=timeout)
     if not res['OK']:
-      res = fcClient.changePathOwner( dict.fromkeys( directories, {'Owner' : user} ) )
-      if not res['OK']:
-        return res
+      res['Action'] = 'changePathOwner'
+      return res
   if group is not None:
-    res = fcClient.changePathGroup( dict.fromkeys( directories, group ) )
+    res = fcClient.changePathGroup(dict.fromkeys(directories, group), recursive=recursive, timeout=timeout)
     if not res['OK']:
-      res = fcClient.changePathGroup( dict.fromkeys( directories, {'Group':group} ) )
-      if not res['OK']:
-        return res
+      res['Action'] = 'changePathGroup'
+      return res
   if mode is not None:
-    res = fcClient.changePathMode( dict.fromkeys( directories, mode ) )
+    res = fcClient.changePathMode(dict.fromkeys(directories, mode), recursive=recursive, timeout=timeout)
     if not res['OK']:
-      res = fcClient.changePathMode( dict.fromkeys( directories, {'Mode':mode} ) )
-      if not res['OK']:
-        return res
-  if recursive:
-    for subDir in directories:
-      if ndirs % 10 == 0:
-        sys.stdout.write( '.' )
-        sys.stdout.flush()
-      ndirs += 1
-      res = fcClient.listDirectory( subDir )
-      if res['OK']:
-        subDirectories = res['Value']['Successful'][subDir]['SubDirs']
-        if subDirectories:
-          # print subDir, len( subDirectories ), ndirs
-          res = chown( subDirectories, user, group = group, mode = mode, recursive = True, ndirs = ndirs, fcClient = fcClient )
-          if not res['OK']:
-            return res
-          ndirs = res['Value']
-  else:
-    ndirs += len( directories )
-  return S_OK( ndirs )
+      res['Action'] = 'changePathMode'
+      return res
 
-def createUserDirectory( user ):
+  ndirs += len(directories)
+  return S_OK(ndirs)
+
+
+def createUserDirectory(user):
   """
   This functions creates (if not existing) a user directory in the DFC
   """
   dfc = FileCatalogClient()
   initial = user[0]
-  baseDir = os.path.join( '/lhcb', 'user', initial, user )
-  if dfc.isDirectory( baseDir ).get( 'Value', {} ).get( 'Successful', {} ).get( baseDir ):
-    return S_ERROR( 'User directory already existing' )
-  gLogger.info( 'Creating directory', baseDir )
-  res = dfc.createDirectory( baseDir )
+  baseDir = os.path.join('/lhcb', 'user', initial, user)
+  if dfc.isDirectory(baseDir).get('Value', {}).get('Successful', {}).get(baseDir):
+    return S_ERROR('User directory already existing')
+  gLogger.info('Creating directory', baseDir)
+  res = dfc.createDirectory(baseDir)
   if not res['OK']:
     return res
-  gLogger.info( 'Setting ownership of directory', baseDir )
-  return chown( baseDir, user, group = 'lhcb_user', mode = 0755, recursive = False, fcClient = dfc )
+  gLogger.info('Setting ownership of directory', baseDir)
+  return chown(baseDir, user, group='lhcb_user', mode=0755, recursive=False, fcClient=dfc)
