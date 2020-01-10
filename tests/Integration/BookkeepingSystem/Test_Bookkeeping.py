b56d4502322ff394b1c00b753d7bea15b62b7cc2
Zoltan Mathe <zoltan.mathe@cern.ch>
2020-01-10 16:27:22 +0100

    fix pylint

--- tests/Integration/BookkeepingSystem/Test_Bookkeeping.py
+++ tests/Integration/BookkeepingSystem/Test_Bookkeeping.py
@@ -987,7 +987,7 @@ class TestMethods(DataInsertTestCase):
     retVal = self.bk.getFiles(bkQuery)
     self.assertTrue(retVal['OK'])
     self.assertEqual(len(retVal['Value']), 125)
-    
+
     bkQuery['EventType'] = [90000000, 91000000]
     retVal = self.bk.getFiles(bkQuery)
     self.assertTrue(retVal['OK'])
