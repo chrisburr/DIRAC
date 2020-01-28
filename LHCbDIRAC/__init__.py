81762042607da1bfccdcc644936e79d3606ca794
Zoltan Mathe <zoltan.mathe@cern.ch>
2020-01-28 09:55:19 +0100

    v9r4p13

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -29,7 +29,7 @@ rootPath = os.path.dirname(os.path.realpath(__path__[0]))
 
 majorVersion = 9
 minorVersion = 4
-patchLevel = 12
+patchLevel = 13
 preVersion = 0
 
 version = "v%sr%s" % (majorVersion, minorVersion)
