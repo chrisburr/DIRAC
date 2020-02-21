26a514fbb0c5ad9f07af9077eef4de34c19ab518
Chris Burr <christopher.burr@cern.ch>
2020-02-21 16:09:16 +0100

    Change IRODSStorageElementHandler to StorageElementHandler for data management tests

--- tests/Jenkins/lhcb_ci.sh
+++ tests/Jenkins/lhcb_ci.sh
@@ -157,7 +157,7 @@ function findRelease(){
 diracServices(){
   echo '==> [diracServices]'
 
-  services=$(cat services |  cut -d '.' -f 1 | grep -Ev '(PilotsLogging|FTSManagerHandler|IRODSStorageElementHandler|^ConfigurationSystem|Plotting|RAWIntegrity|RunDBInterface|ComponentMonitoring|WMSSecureGW)' | sed -e 's/System / /g' -e 's/Handler//g' -e 's/ /\//g')
+  services=$(cat services |  cut -d '.' -f 1 | grep -Ev '(PilotsLogging|FTSManagerHandler|StorageElementHandler|^ConfigurationSystem|Plotting|RAWIntegrity|RunDBInterface|ComponentMonitoring|WMSSecureGW)' | sed -e 's/System / /g' -e 's/Handler//g' -e 's/ /\//g')
 
   for serv in $services
   do
