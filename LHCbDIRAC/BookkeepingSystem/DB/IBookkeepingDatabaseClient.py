89e13365903ac2b16c02bb30598889a398ef3aa5
Zoltan Mathe <zoltan.mathe@cern.ch>
2020-03-24 16:58:08 +0100

    fix code style

--- LHCbDIRAC/BookkeepingSystem/DB/IBookkeepingDatabaseClient.py
+++ LHCbDIRAC/BookkeepingSystem/DB/IBookkeepingDatabaseClient.py
@@ -105,7 +105,8 @@ class IBookkeepingDatabaseClient(object):
                                             processing, evt, visible, ftype, replicaFlag)
 
   #############################################################################
-  def getFileTypes(self, configName, configVersion, conddescription, processing, evt, runnb, production, visible, replicaFlag):
+  def getFileTypes(self, configName, configVersion, conddescription, processing,
+                   evt, runnb, production, visible, replicaFlag):
     "more info in the BookkeepingClient.py"
     return self.getManager().getFileTypes(configName, configVersion,
                                           conddescription, processing,
