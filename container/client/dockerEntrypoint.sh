23a54823d2e3578dd325c2d50c1e72c8b1e6c62f
Federico Stagni <federico.stagni@cern.ch>
2020-04-20 15:39:07 +0200

    use root (needs user namespaces)

--- container/client/dockerEntrypoint.sh
+++ container/client/dockerEntrypoint.sh
@@ -13,14 +13,14 @@
 # Entry point of docker to setup the DIRAC environment before executing the command
 
 source /opt/dirac/bashrc
-cd /home/dirac
+cd /root
 
 echo "******************************************************************************************"
 echo "====> LHCbDIRAC client"
 echo ""
 echo "You should have started this container bind-mounting a dir with your cert pem files"
 echo "e.g. with"
-echo "  docker run -it -v /local/dir/to/user/certificate/pem/files:/home/dirac/.globus lhcbdirac:vXrY bash"
+echo "  docker run -it -v /local/dir/to/user/certificate/pem/files:/root/.globus gitlab-registry.cern.ch/lhcb-dirac/lhcbdirac:client-vXrY bash"
 echo ""
 echo "If you have done so, create a proxy and work normally"
 echo ""
