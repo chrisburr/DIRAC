c6da50c621325322cdb0034a987ec5d4fe1b7f8c
Aymane Leyli <aymane.leyli@cern.ch>
2020-06-17 07:33:59 +0000

    Update UploadMC.py

--- LHCbDIRAC/Workflow/Modules/UploadMC.py
+++ LHCbDIRAC/Workflow/Modules/UploadMC.py
@@ -79,7 +79,7 @@ class UploadMC(ModuleBase):
 
 
       # looking for xml files that are 'summaryGauss_self.production_id_self.prod_job_id_1.xml'
-      xmlfl = 'summaryGauss_%s_%s_1.xml' % (self.production_id, self.jobID)
+      xmlfl = 'summaryGauss_%s_%s_1.xml' % (self.production_id, self.prod_job_id)
       if os.path.exists(xmlfl):
         try:
           xmlData = XMLSummary(xmlfl)
