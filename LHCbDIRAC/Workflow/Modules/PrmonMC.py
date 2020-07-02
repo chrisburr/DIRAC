db88ce6ae4a6923525b4d86b251c4ac355b14ba6
Aymane <leyliaymane@gmail.com>
2020-07-02 18:35:29 +0200

    Fix some errors in PrmonMC.py

--- LHCbDIRAC/Workflow/Modules/PrmonMC.py
+++ LHCbDIRAC/Workflow/Modules/PrmonMC.py
@@ -33,7 +33,7 @@ class PrmonMC(ModuleBase):
 
     self.version = __RCSID__
 
-  def createData():
+  def createData(self):
     """Method for creating the metrics data of an application."""
 
     if self.applicationPID:
@@ -41,13 +41,13 @@ class PrmonMC(ModuleBase):
       fileName = 'prmon_%s_%s_%s' % (self.applicationName, self.production_id, self.prod_job_id)
 
       # Specifying the command that runs prmon given the PID of the application
-      cmdPRMON =  "prmon --pid %s --filename %s.txt --json-summary %s.json" % (self.applicationPID, fileName, fileName)
-      os.system(cmd)
+      cmdPRMON = "prmon --pid %s --filename %s.txt --json-summary %s.json" % (self.applicationPID, fileName, fileName)
+      os.system(cmdPRMON)
       # this command will end once the application process is no longer running
 
       with open(fileName + '.json') as JS:
         self.jsonData = json.load(JS)
-        self.log.verbose("Content of JSON file", "%s: %s" % (fileName + '.json', jsonData))
+        self.log.verbose("Content of JSON file", "%s: %s" % (fileName + '.json', self.jsonData))
 
         # Enriching the jsonData with the IDS
         ids = dict()
@@ -60,14 +60,14 @@ class PrmonMC(ModuleBase):
     else:
       self.log.info("PID not found, no data to create")
 
-  def uploadData():
+  def uploadData(self):
     """Method for uploading the metrics data of the application to ES."""
 
     if self.applicationPID:
       mcMetricsClient = MCStatsClient()
       mcMetricsClient.indexName = 'lhcb-mcstats-Metrics-' + self.production_id
-      res = mcMectricsClient.set('mcMetrics', self.jsonData)
+      res = mcMetricsClient.set('mcMetrics', self.jsonData)
       if not res['OK']:
-        self.log.error('the application\'s Metrics data not set, exiting without affecting workflow status', "%s: %s" % (str(jsonData), res['Message']))  # nopep8
+        self.log.error('the application\'s Metrics data not set, exiting without affecting workflow status', "%s: %s" % (str(self.jsonData), res['Message']))  # nopep8
     else:
       self.log.info("PID not found, no data to upload")
