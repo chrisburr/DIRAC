9072430c5ae31d0ab520f257e83cadf0499c210e
fstagni <federico.stagni@cern.ch>
2020-07-03 18:51:19 +0200

    moved tests to server

--- tests/Integration/all_integration_client_tests.sh
+++ tests/Integration/all_integration_client_tests.sh
@@ -21,11 +21,6 @@ echo -e '****************************************'
 echo -e '******' "LHCb client -> server tests" '******\n'
 
 #-------------------------------------------------------------------------------#
-echo -e "*** $(date -u) **** LHCb Bookkeeping TESTS ****\n"
-python "$CLIENTINSTALLDIR/LHCbDIRAC/tests/Integration/BookkeepingSystem/Test_Bookkeeping.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-python "$CLIENTINSTALLDIR/LHCbDIRAC/tests/Integration/BookkeepingSystem/Test_BookkeepingGUImethods.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-
-#-------------------------------------------------------------------------------#
 echo -e "*** $(date -u) **** LHCb PMS TESTS ****\n"
 python "$CLIENTINSTALLDIR/LHCbDIRAC/tests/Integration/ProductionManagementSystem/Test_ProductionRequest.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 python "$CLIENTINSTALLDIR/LHCbDIRAC/tests/Integration/ProductionManagementSystem/Test_Client_MCStatsElasticDB.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
