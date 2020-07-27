30f30c24eeb3174f0298f1a0d47741b9a33bee78
Marko Petric <marko.petric@cern.ch>
2020-07-27 15:08:16 +0200

    Updte version to v10r0p9 and add release notes

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -29,7 +29,7 @@ rootPath = os.path.dirname(os.path.realpath(__path__[0]))
 
 majorVersion = 10
 minorVersion = 0
-patchLevel = 8
+patchLevel = 9
 preVersion = 0
 
 version = "v%sr%s" % (majorVersion, minorVersion)
