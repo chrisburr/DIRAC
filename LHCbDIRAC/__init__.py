9cff80a14e7a1601b4a137a9c666fcbdcd64d1ca
Marko Petric <marko.petric@cern.ch>
2020-07-28 11:03:14 +0200

    Add v10r0p10 release to devel cfg

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -29,7 +29,7 @@ rootPath = os.path.dirname(os.path.realpath(__path__[0]))
 
 majorVersion = 10
 minorVersion = 0
-patchLevel = 9
+patchLevel = 10
 preVersion = 0
 
 version = "v%sr%s" % (majorVersion, minorVersion)
