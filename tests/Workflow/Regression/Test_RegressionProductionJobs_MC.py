01f5a8b17ffc7221124414d8fbb35f0cb3717e72
Aymane <leyliaymane@gmail.com>
2020-08-07 17:24:10 +0200

    Add more tests

--- tests/Workflow/Regression/Test_RegressionProductionJobs_MC.py
+++ tests/Workflow/Regression/Test_RegressionProductionJobs_MC.py
@@ -51,10 +51,10 @@ class MCSuccess(RegressionTestCase):
 
     res = j_mc_40651.runLocal(self.diracLHCb)
     self.assertTrue(res['OK'])
-  
+
   def test_Regression_Production2(self):
 
-    location123456 = find_all('40651.xml', '..', 'Regression')[0]
+    location123456 = find_all('123456.xml', '..', 'Regression')[0]
     j_mc_123456 = LHCbJob(location123456)
     j_mc_123456.setConfigArgs('pilot.cfg')
 
