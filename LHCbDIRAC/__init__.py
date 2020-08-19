1680f1bedc3ef30f0bbbb8a1e4edb65c476b28ae
Christophe <christophe.haen@cern.ch>
2020-08-19 18:02:36 +0200

    v10r1-pre9

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -30,7 +30,7 @@ rootPath = os.path.dirname(os.path.realpath(__path__[0]))
 majorVersion = 10
 minorVersion = 1
 patchLevel = 0
-preVersion = 8
+preVersion = 9
 
 version = "v%sr%s" % (majorVersion, minorVersion)
 buildVersion = "v%dr%d" % (majorVersion, minorVersion)
