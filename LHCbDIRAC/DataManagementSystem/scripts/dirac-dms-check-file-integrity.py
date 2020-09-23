26bd8627f37dc5b6c24177fc639b3979618e790a
Federico Stagni <federico.stagni@cern.ch>
2020-09-23 16:22:53 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-check-file-integrity.py
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-check-file-integrity.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
   lfns = dmScript.getOption('LFNs')
   if not lfns:
     print("No LFNs given...")
-    Script.showHelp()
+    Script.showHelp(exitCode=1)
 
   integrityClient = DataIntegrityClient()
   res = integrityClient.catalogFileToBK(lfns)
