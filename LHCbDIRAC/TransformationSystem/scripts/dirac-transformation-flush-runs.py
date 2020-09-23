97f289310351ec4a8083124bcf2234b24c22570b
Federico Stagni <federico.stagni@cern.ch>
2020-09-23 16:23:47 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- LHCbDIRAC/TransformationSystem/scripts/dirac-transformation-flush-runs.py
+++ LHCbDIRAC/TransformationSystem/scripts/dirac-transformation-flush-runs.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
 
   if len(args) != 1:
     gLogger.fatal("Specify transformation number...")
-    Script.showHelp()
+    Script.showHelp(exitCode=1)
   else:
     ids = args[0].split(",")
     idList = []
