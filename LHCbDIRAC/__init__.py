cc4ee3da34fd1a1aea403a0fe5261c5ac38ec030
Marko Petric <marko.petric@cern.ch>
2021-01-25 17:21:09 +0100

    set __init__.py for pre1

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -28,9 +28,9 @@ rootPath = os.path.dirname(os.path.realpath(__path__[0]))
 # Define Version
 
 majorVersion = 10
-minorVersion = 1
-patchLevel = 1
-preVersion = 0
+minorVersion = 2
+patchLevel = 0
+preVersion = 1
 
 version = "v%sr%s" % (majorVersion, minorVersion)
 buildVersion = "v%dr%d" % (majorVersion, minorVersion)
