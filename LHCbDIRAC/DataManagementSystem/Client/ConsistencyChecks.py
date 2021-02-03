8c3ce0cc01172b50ab78c1b244fb34ddec5d686d
Christophe Haen <christophe.haen@cern.ch>
2021-02-03 18:33:51 +0100

    ConsistencyChecks: replace deprecated DataManager method with direct FileCatalog call

--- LHCbDIRAC/DataManagementSystem/Client/ConsistencyChecks.py
+++ LHCbDIRAC/DataManagementSystem/Client/ConsistencyChecks.py
@@ -909,7 +909,7 @@ class ConsistencyChecks(DiracConsistencyChecks):
         else:
           printout = True
           topDir = os.path.dirname(directory)
-          res = self.dataManager.getCatalogListDirectory(topDir)
+          res = self.fileCatalog.listDirectory(topDir)
           if not res['OK']:
             raise RuntimeError("Error listing directory: " + res['Message'])
           else:
