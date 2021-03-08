afba29896cf87e9f9615640f2e027dcce0f910be
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2021-03-08 09:29:44 +0100

    use os.path rather than string manipulation

--- LHCbDIRAC/DataManagementSystem/DB/StorageUsageDB.py
+++ LHCbDIRAC/DataManagementSystem/DB/StorageUsageDB.py
@@ -16,6 +16,7 @@
 :synopsis: StorageUsageDB class is a front-end to the Storage Usage Database.
 """
 
+import os
 # # from DIRAC
 from DIRAC import S_OK, S_ERROR
 from DIRAC.Core.Base.DB import DB
@@ -24,7 +25,7 @@ __RCSID__ = "$Id$"
 
 
 def _standardDirectory(dirPath):
-  return dirPath if dirPath[-1] == '/' else dirPath + '/'
+  return os.path.join(dirPath, '')
 
 #############################################################################
 
@@ -147,9 +148,7 @@ class StorageUsageDB(DB):
   def __stripDirectory(self, dirPath):
     """Remove trailing / in directory names."""
     dirPath = self._escapeString(dirPath)['Value'][1:-1]
-    while dirPath and dirPath[-1] == '/':
-      dirPath = dirPath[:-1]
-    return dirPath
+    return os.path.realpath(dirPath)
 
   ################
   # Bulk insertion
