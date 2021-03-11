bb5a849a3f4afa7bd8799b6b9144c411650d2d2a
Chris Pawley <cpawley@cern.ch>
2021-03-11 23:07:24 +0100

    Fix pycodestyle error

--- LHCbDIRAC/ProductionManagementSystem/Utilities/Utils.py
+++ LHCbDIRAC/ProductionManagementSystem/Utilities/Utils.py
@@ -100,7 +100,8 @@ def informPeople(rec, oldstate, state, author, inform):
   footer += "DIRAC Web portal: https://lhcb-portal-dirac.cern.ch/DIRAC/s:%s/g:" % \
       PathFinder.getDIRACSetup()
 
-  ppath = "/?view=tabs&theme=Crisp&url_state=1|*LHCbDIRAC.ProductionRequestManager.classes.ProductionRequestManager: \n\n"
+  ppath = .join(['/?view=tabs&theme=Crisp&url_state=1|*LHCbDIRAC.ProductionRequestManager',
+                 '.classes.ProductionRequestManager: \n\n'])
 
   ppath += 'The request details:\n'
   ppath += '  Type: %s' % str(rec['RequestType'])
