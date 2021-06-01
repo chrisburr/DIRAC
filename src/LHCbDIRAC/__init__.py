898ef9f659094da963403fc416fd2ced067c09f5
Marko Petric <marko.petric@cern.ch>
2021-06-01 15:58:11 +0200

    Revert wrong tag

--- src/LHCbDIRAC/__init__.py
+++ src/LHCbDIRAC/__init__.py
@@ -42,8 +42,8 @@ if six.PY3:
     version = "Unknown"
 else:
   majorVersion = 10
-  minorVersion = 1
-  patchLevel = 16
+  minorVersion = 2
+  patchLevel = 0
   preVersion = 0
 
   version = "v%sr%s" % (majorVersion, minorVersion)
