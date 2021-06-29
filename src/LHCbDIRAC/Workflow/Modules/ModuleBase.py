4f576151f5a0efb0a6b4b3a7bc555d4393a1ca0c
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2021-06-29 13:31:11 +0200

    protect against bad runMetadata

--- src/LHCbDIRAC/Workflow/Modules/ModuleBase.py
+++ src/LHCbDIRAC/Workflow/Modules/ModuleBase.py
@@ -281,9 +281,10 @@ class ModuleBase(object):
 
     if 'runMetadata' in self.workflow_commons:
       runMetadataDict = eval(self.workflow_commons['runMetadata'])
-      self.onlineDDDBTag = runMetadataDict['DDDB']
-      self.onlineCondDBTag = runMetadataDict['CondDb']
-      self.TCK = runMetadataDict['TCK']
+      # If the information is absent, set to None
+      self.onlineDDDBTag = runMetadataDict.get('DDDB')
+      self.onlineCondDBTag = runMetadataDict.get('CondDb')
+      self.TCK = runMetadataDict.get('TCK')
 
     if 'outputDataFileMask' in self.workflow_commons:
       self.outputDataFileMask = self.workflow_commons['outputDataFileMask']
