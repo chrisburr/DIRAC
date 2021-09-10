1d29f329b8c0e987a74e99e96dda5530e27fda9c
Christophe Haen <christophe.haen@cern.ch>
2021-09-10 18:46:13 +0200

    TS: import WorkflowTask from the new location

--- src/LHCbDIRAC/TransformationSystem/Client/TaskManager.py
+++ src/LHCbDIRAC/TransformationSystem/Client/TaskManager.py
@@ -13,7 +13,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from DIRAC.TransformationSystem.Client.TaskManager import WorkflowTasks
+from DIRAC.TransformationSystem.Client.WorkflowTasks import WorkflowTasks
 
 __RCSID__ = "$Id$"
 
