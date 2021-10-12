6b907e97a79f132cf36204d7accfceaf6cad8f33
Federico Stagni <federico.stagni@cern.ch>
2021-10-12 18:43:02 +0200

    Merge branch 'fix_master' into 'master'
    Fix indentation of createReleaseFiles
    
    See merge request lhcb-dirac/LHCbDIRAC!1096

--- src/LHCbDIRAC/tests/Utilities/createReleaseFiles.py
+++ src/LHCbDIRAC/tests/Utilities/createReleaseFiles.py
@@ -117,12 +117,12 @@ with open("series.txt", "a") as fser:
 # change the __init__.py
 # Currently master and devel have different layout
 if not preRelease:
-    lineReplace("LHCbDIRAC/__init__.py", 40, "  majorVersion = %s" % version[0])
-    lineReplace("LHCbDIRAC/__init__.py", 41, "  minorVersion = %s" % version[1])
-    lineReplace("LHCbDIRAC/__init__.py", 42, "  patchLevel = %s" % version[2])
-    lineReplace("LHCbDIRAC/__init__.py", 43, "  preVersion = %s" % 0)
+    lineReplace("src/LHCbDIRAC/__init__.py", 45, "    majorVersion = %s" % version[0])
+    lineReplace("src/LHCbDIRAC/__init__.py", 46, "    minorVersion = %s" % version[1])
+    lineReplace("src/LHCbDIRAC/__init__.py", 47, "    patchLevel = %s" % version[2])
+    lineReplace("src/LHCbDIRAC/__init__.py", 48, "    preVersion = %s" % 0)
 else:
-    lineReplace("src/LHCbDIRAC/__init__.py", 43, "  majorVersion = %s" % version[0])
-    lineReplace("src/LHCbDIRAC/__init__.py", 44, "  minorVersion = %s" % version[1])
-    lineReplace("src/LHCbDIRAC/__init__.py", 45, "  patchLevel = %s" % 0)
-    lineReplace("src/LHCbDIRAC/__init__.py", 46, "  preVersion = %s" % version[3])
+    lineReplace("src/LHCbDIRAC/__init__.py", 45, "    majorVersion = %s" % version[0])
+    lineReplace("src/LHCbDIRAC/__init__.py", 46, "    minorVersion = %s" % version[1])
+    lineReplace("src/LHCbDIRAC/__init__.py", 47, "    patchLevel = %s" % 0)
+    lineReplace("src/LHCbDIRAC/__init__.py", 48, "    preVersion = %s" % version[3])
