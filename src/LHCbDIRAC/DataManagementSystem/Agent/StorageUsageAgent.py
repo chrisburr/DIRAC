0cd4fb4b671a550cb442f062451178398801c3fd
fstagni <federico.stagni@cern.ch>
2022-04-04 12:55:10 +0200

    fix: removed gMonitor following removal in DIRAC

--- src/LHCbDIRAC/DataManagementSystem/Agent/StorageUsageAgent.py
+++ src/LHCbDIRAC/DataManagementSystem/Agent/StorageUsageAgent.py
@@ -30,7 +30,6 @@ from DIRAC.ConfigurationSystem.Client.Helpers import Registry
 from DIRAC.Core.Utilities.DirectoryExplorer import DirectoryExplorer
 from DIRAC.Core.Utilities.File import mkDir
 from DIRAC.FrameworkSystem.Client.ProxyManagerClient import gProxyManager
-from DIRAC.FrameworkSystem.Client.MonitoringClient import gMonitor
 from DIRAC.Resources.Catalog.FileCatalog import FileCatalog
 from DIRAC.Core.Utilities.ReturnValues import returnSingleResult
 from DIRAC.Core.Utilities import List
@@ -226,33 +225,12 @@ class StorageUsageAgent(AgentModule):
             self.log.notice("============================================================")
             self.log.notice("%-40s %20s %20s" % ("Storage Element", "Number of files", "Total size"))
 
-            for se in sorted(res["Value"]):
-                site = se.split("_")[0].split("-")[0]
-                gMonitor.registerActivity(
-                    "%s-used" % se,
-                    "%s usage" % se,
-                    "StorageUsage/%s usage" % site,
-                    "",
-                    gMonitor.OP_MEAN,
-                    bucketLength=600,
-                )
-                gMonitor.registerActivity(
-                    "%s-files" % se,
-                    "%s files" % se,
-                    "StorageUsage/%s files" % site,
-                    "Files",
-                    gMonitor.OP_MEAN,
-                    bucketLength=600,
-                )
-
             time.sleep(2)
 
             for se in sorted(res["Value"]):
                 usage = res["Value"][se]["Size"]
                 files = res["Value"][se]["Files"]
                 self.log.notice("%-40s %20s %20s" % (se, str(files), str(usage)))
-                gMonitor.addMark("%s-used" % se, usage)
-                gMonitor.addMark("%s-files" % se, files)
 
     def execute(self):
         """execution in one cycle."""
