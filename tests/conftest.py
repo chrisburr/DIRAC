cc81a3e5a724509dd502902b67067a172d5cc61f
Chris Burr <christopher.burr@cern.ch>
2022-08-16 11:59:57 +0200

    test: Ensure DIRAC isn't configured before running unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,9 @@
+import os
+from pathlib import Path
+
 import pytest
+
+import DIRAC
 
 
 # Adds the --runslow command line arg based on the example in the docs
@@ -20,3 +25,18 @@ def pytest_collection_modifyitems(config, items):
     for item in items:
         if "slow" in item.keywords:
             item.add_marker(skip_slow)
+
+
+def check_environment():
+    """Ensure the environment is safe for running tests"""
+    dirac_cfg = Path(DIRAC.rootPath) / "etc" / "dirac.cfg"
+    if dirac_cfg.exists():
+        raise EnvironmentError(f"{dirac_cfg}")
+    user_proxy_path = Path("/tmp") / f"x509up_u{os.getuid()}"
+    if user_proxy_path.exists():
+        raise EnvironmentError(f"Found possible proxy file at {user_proxy_path}")
+    if "X509_USER_PROXY" in os.environ:
+        raise EnvironmentError(f"X509_USER_PROXY is set")
+
+
+check_environment()
